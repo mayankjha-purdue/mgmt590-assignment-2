b5ef428489c47f505f1df647c88b9d984d58923e
Mayank Jha <78186044+mayankjha-purdue@users.noreply.github.com>
2021-05-26 20:31:33 -0400

    Update answer.py
    Added db code in the main file

--- answer.py
+++ answer.py
@@ -3,11 +3,38 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from transformers.pipelines import pipeline
-from db import create_tables
-from db import get_db
 import os
 
 global modelList
+import sqlite3
+DATABASE_NAME = "prodscale.db"
+
+
+def get_db():
+    conn = sqlite3.connect(DATABASE_NAME)
+    return conn
+
+
+def create_tables():
+    tables = [
+        """CREATE TABLE IF NOT EXISTS prodscale(
+                timestamp INTEGER PRIMARY KEY,
+                model TEXT NOT NULL,
+                answer TEXT NOT NULL,
+                question TEXT NOT NULL,
+				context TEXT NOT NULL)
+            """
+    ]
+    db = get_db()
+    cursor = db.cursor()
+    for table in tables:
+        cursor.execute(table)
+
+
+
+
+
+
 modelList = [
     {
         'name': "distilled-bert",
